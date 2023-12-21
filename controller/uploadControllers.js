7e7c7bd25e189886b69743a368b03fb9cb60b238
parvsehgal <parvsehgal2004@gmail.com>
2023-12-21 22:59:45 +0530

    added route that lets us to reduce image quality

--- controller/uploadControllers.js
+++ controller/uploadControllers.js
@@ -1,8 +1,12 @@
 const File = require("../models/file");
 const cloudinary = require("cloudinary").v2;
 
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder, quality) {
   const options = { folder };
+  if (quality) {
+    options.quality = quality;
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+  }
   return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
@@ -28,6 +32,21 @@ exports.imageUpload = async (req, res) => {
     res.status(500).json("file format not supported");
   }
   const response = await uploadFileToCloudinary(file, "myFolder");
+  const ourFileInDb = await File.create({
+    title: file.name,
+    url: response.url,
+  });
+  res.status(200).json(ourFileInDb);
+};
+
+exports.imageUploadReducer = async (req, res) => {
+  const { quality } = req.body;
+  const file = req.files.ourFile;
+  const ext = file.name.split(".")[1];
+  if (ext !== "jpg" && ext !== "jpeg" && ext !== "png") {
+    res.status(500).json("file format not supported");
+  }
+  const response = await uploadFileToCloudinary(file, "myFolder", quality);
   const ourFileInDb = await File.create({
     title: file.name,
     url: response.url,
--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   uploadHandler,
   imageUpload,
+  imageUploadReducer,
 } = require("../controller/uploadControllers");
 
 router.post("/fileUpload", uploadHandler);
 router.post("/imageUpload", imageUpload);
+router.post("/imageUploadReduce", imageUploadReducer);
 
 module.exports = router;
